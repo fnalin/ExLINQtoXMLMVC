180a4c64b616e00afebca512920f635bc85f0451
fnalin <fnalin@msn.com>
2013-12-13 18:51:04 -0400

    subindo o projeto

---
+++ UI.MVC/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using UI.MVC.Models;
+
+namespace UI.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //stackoverflow.com/questions/3063614/xelement-load-app-data-file-xml-could-not-find-a-part-of-the-path
+            string filePath = Path.Combine(
+                        HostingEnvironment.ApplicationPhysicalPath,
+                        @"App_Data\Contatos.xml"
+                    );
+
+            var element = XElement.Load(filePath);
+            var query = from member in element.Descendants("contato")
+                        select new Contato
+                        {
+                            ID = Guid.Parse(member.Attribute("id").Value),
+                            Nome = member.Element("nome").Value,
+                            Sobrenome = member.Element("sobrenome").Value,
+                            Email = member.Element("email").Value,
+                            Telefone = member.Element("telefone").Value
+                        };
+
+            return View(query);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ UI.MVC/Models/Contato.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UI.MVC.Models
+{
+    public class Contato
+    {
+        public Guid ID { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+    }
+}
